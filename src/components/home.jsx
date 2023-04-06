1014cb5f4ab05146fbc4cac6543ba5d283785310
Wubaboo <evan4217@gmail.com>
2023-04-06 00:57:45 -0400

    add mosaic card

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,22 @@
-import Card from "./card"
-import CARDS from "../data/gameCards"
-import "../styles/home.css"
+import Card from "./card";
+import CARDS from "../data/gameCards";
+import "../styles/home.css";
 
-export default function Home (props) {
-    return (
-        <div className="home">
-            <div className="cards">
-                {CARDS.map((card, i) => (<Card key={i} label={card.label} imagePath={card.imagePath} caption={card.caption} alt={card.alt} path={card.path}></Card>))  }
-            </div>
-        </div>
-    )
+export default function Home(props) {
+  return (
+    <div className="home">
+      <div className="cards">
+        {CARDS.map((card, i) => (
+          <Card
+            key={i}
+            label={card.label}
+            imagePath={card.imagePath}
+            caption={card.caption}
+            alt={card.alt}
+            path={card.path}
+          ></Card>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/data/gameCards.jsx
+++ src/data/gameCards.jsx
@@ -1,19 +1,25 @@
 const cards = [
-    {
-        label: "Numbers",
-      alt: "Number Game screenshot",
-      caption: "Click the numbers in increasing order",
-      imagePath: process.env.PUBLIC_URL + "/assets/numbers.png",
-      path: "/numbers/"
-    },
-    {
-        label: "Matching",
-      alt: "Matching Game screenshot",
-      caption: "Match the pairs of images",
-      imagePath: process.env.PUBLIC_URL + "/assets/matching.png",
-      path: "/matching/"
-    },
-  ];
-  
-  export default cards;
-  
+  {
+    label: "Numbers",
+    alt: "Number Game screenshot",
+    caption: "Click the numbers in increasing order",
+    imagePath: process.env.PUBLIC_URL + "/assets/numbers.png",
+    path: "/numbers/",
+  },
+  {
+    label: "Matching",
+    alt: "Matching Game screenshot",
+    caption: "Match the pairs of images",
+    imagePath: process.env.PUBLIC_URL + "/assets/matching.png",
+    path: "/matching/",
+  },
+  {
+    label: "Mosaic",
+    alt: "Mosaic Game screenshot",
+    caption: "Recreate the pattern",
+    imagePath: process.env.PUBLIC_URL + "/assets/mosaic.png",
+    path: "/mosaic/",
+  },
+];
+
+export default cards;
