fc6f047b9d1b7056f120cc9a90019a43988e68f0
Wubaboo <evan4217@gmail.com>
2023-04-14 22:07:28 -0400

    change mosaic colours

--- src/data/colors.jsx
+++ src/data/colors.jsx
@@ -1,15 +1,27 @@
+// const colors = [
+//   "white",
+//   "red",
+//   "blue",
+//   "yellow",
+//   "green",
+//   "purple",
+//   "orange",
+//   "navy",
+//   "pink",
+//   "black",
+//   "brown",
+// ];
+
 const colors = [
   "white",
-  "red",
-  "blue",
-  "yellow",
-  "green",
-  "purple",
-  "orange",
-  "navy",
-  "pink",
-  "black",
-  "brown",
+  "#F47C7C",
+  "#70A1D7",
+  "#F7F48B",
+  "#A1DE93",
+  "#B08BBB",
+  "#ECA869",
+  "#2A2F4F",
+  "#FDE2F3",
 ];
 
 export default colors;
